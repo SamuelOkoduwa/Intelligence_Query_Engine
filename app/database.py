d446186dd08be0cb8256bcde21087757893657b1
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2026-04-25 23:42:28 +0100

    Add Neon Postgres SSL support

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./profiles.db")
 
-engine = create_engine(DATABASE_URL, future=True)
+_connect_args = {}
+if DATABASE_URL.startswith("postgresql"):
+    _connect_args = {"sslmode": "require"}
+
+engine = create_engine(DATABASE_URL, future=True, connect_args=_connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 
 
